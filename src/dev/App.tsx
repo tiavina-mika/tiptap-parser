33b100020cb9bb1f7e4ab3beed5aa40834999431
tiavina-mika <tiavinamika@gmail.com>
2024-05-30 11:59:47 +0300

    Update App.tsx

--- src/dev/App.tsx
+++ src/dev/App.tsx
@@ -1,70 +1,48 @@
-import parseHtml, { HtmlCodeParser } from '../Parse'
-import { parse as nodeParse } from 'node-html-parser';
-
-// const html = `
-// <h1>Here is an exemple of code</h1>
-// <p>This is a stringified html with code</p>
-// <br/>
-// <pre>
-// <code>
-// const App = () => {
-//   const [open, setOpen] = useState<boolean>(false);
-
-//   const handleOpen = () => setOpen(!open);
-
-//   return (
-//     <div className="bg-error">
-//       {parseHtml(html, { language: 'tsx' })}
-//       <HtmlCodeParser language="tsx" codeContainerClassName="custom-class">
-//         {html}
-//       </HtmlCodeParser>
-//       <button onClick={handleOpen}>
-//         {open ? 'Close' : 'Open'}
-//       </button>
-//       </ br>
-//     </div>
-//   )
-// }
-// </code></pre>
-
-// `;
+import TiptapParser from "../Parse";
 
 const html = `
 <h1>Here is an exemple of code</h1>
 <p>This is a stringified html with code</p>
 <br/>
-<pre>
-<code>
-const App = () => {
-  return (
-    <div>
-    <MyComponent className="cool />
+<pre><code>import Link from 'next/link';
 
-    </div>
+import Title from '@/components/typography/Title';
 
-  )
-}
-</code></pre>
+// some comment here
 
+const NotFound = () =&gt; (
+  &lt;html lang="en"&gt;
+    &lt;body className=""&gt;
+      &lt;div className="flex min-h-screen flex-col items-center justify-center space-y-8"&gt;
+        &lt;Title className="text-4xl font-semibold"&gt;404 - Page Not Found&lt;/Title&gt;
+
+        &lt;div className="space-x-4"&gt;
+          &lt;Link
+            className="text-blue-600 underline duration-300 hover:text-red-500"
+            href="/"
+          &gt;
+            Homepage
+          &lt;/Link&gt;
+          &lt;Link
+            className="text-blue-600 underline duration-300 hover:text-red-500"
+            href="/contact"
+          &gt;
+            Contact Us
+          &lt;/Link&gt;
+        &lt;/div&gt;
+      &lt;/div&gt;
+    &lt;/body&gt;
+  &lt;/html&gt;
+);
+export default NotFound;
+</code></pre><p></p>
 `;
-// const root = nodeParse(html);
-// console.log('root: ', root);
-// console.log(root.getElementsByTagName('pre'));
-// console.log('root: ', root);
-// root.replaceWith((node) => {
-//   console.log('node: ', node);
-//   return node
-
-// })
 
 const App = () => {
   return (
-    <div>
-      {parseHtml(html, { language: 'tsx' })}
-      <HtmlCodeParser language="tsx" codeContainerClassName="custom-class">
-        {html}
-      </HtmlCodeParser>
-    </div>
+    <TiptapParser language="tsx" classNames={{ h1ClassName: 'h1'}}>
+      {html}
+    </TiptapParser>
   )
 }
 
