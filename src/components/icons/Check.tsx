9dd02ccbb0729d8994e32e9a6ddff39e73320482
tiavina-mika <tiavinamika@gmail.com>
2024-10-09 11:28:54 +0300

    Create Check.tsx

---
+++ src/components/icons/Check.tsx
@@ -0,0 +1,10 @@
+
+const Check = () => {
+  return (
+    <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+      <polyline points="20 6 9 17 4 12" />
+    </svg>
+  );
+}
+
+export default Check;
